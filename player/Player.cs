870770ad8583eadc9ca5db35989a1c179b961ee9
KippTheKippster <vaskostoulas@gmail.com>
2022-10-13 13:56:11 +0200

    Re-add Player movement

--- player/Player.cs
+++ player/Player.cs
@@ -3,19 +3,32 @@ using System;
 
 public class Player : KinematicBody2D
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
+    Vector2 velocity;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		
-	}
+    const float gravity = 982F;
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    int inputX;
+
+    public override void _Process(float delta)
+    {
+       ReadInput(); 
+       Move(delta);
+    }
+
+    private void ReadInput()
+    {
+        inputX = 0;
+        if (Input.IsActionPressed("right")) 
+            inputX = 1;
+        if (Input.IsActionPressed("left"))
+            inputX = -1;
+    }
+
+    private void Move(float delta)
+    {
+        velocity = new Vector2(inputX * 100, velocity.y);
+        velocity.y += gravity * delta;
+
+        MoveAndSlide(velocity, Vector2.Up);
+    }
 }
