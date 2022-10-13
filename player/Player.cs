32467d5a0de1eba35d9d429a495635a6a430b9f7
LightBlueNavy <imateorjuela@gmail.com>
2022-10-13 14:04:17 +0200

    add stair way to heaven

--- player/Player.cs
+++ player/Player.cs
@@ -3,32 +3,32 @@ using System;
 
 public class Player : KinematicBody2D
 {
-    Vector2 velocity;
+	Vector2 velocity;
 
-    const float gravity = 982F;
+	const float gravity = 982F;
 
-    int inputX;
+	int inputX;
 
-    public override void _Process(float delta)
-    {
-       ReadInput(); 
-       Move(delta);
-    }
+	public override void _Process(float delta)
+	{
+	   ReadInput(); 
+	   Move(delta);
+	}
 
-    private void ReadInput()
-    {
-        inputX = 0;
-        if (Input.IsActionPressed("right")) 
-            inputX = 1;
-        if (Input.IsActionPressed("left"))
-            inputX = -1;
-    }
+	private void ReadInput()
+	{
+		inputX = 0;
+		if (Input.IsActionPressed("right")) 
+			inputX = 1;
+		if (Input.IsActionPressed("left"))
+			inputX = -1;
+	}
 
-    private void Move(float delta)
-    {
-        velocity = new Vector2(inputX * 100, velocity.y);
-        velocity.y += gravity * delta;
+	private void Move(float delta)
+	{
+		velocity = new Vector2(inputX * 100, velocity.y);
+		velocity.y += gravity * delta;
 
-        MoveAndSlide(velocity, Vector2.Up);
-    }
+		MoveAndSlide(velocity, Vector2.Up);
+	}
 }
